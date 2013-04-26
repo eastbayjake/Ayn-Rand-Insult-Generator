0c7f3f5891e64c8d83bc58ea790f59b4489b9c58
Jake McGuire <jakemcguire2@gmail.com>
2013-04-26 15:59:04 -0500

    Now with the actual Javascript code!
    I originally wrote this in Ruby, but now it's in Javascript.

---
+++ aynrandinsultgenerator.js
@@ -0,0 +1,13 @@
+var adj = ["lousy", "cheap", "awful", "miserable", "touchy", "abysmal", "driveling"];
+var noun = ["bastard", "monstrosity", "mystic", "scum", "non-entity", "old fool", "social-metaphysical mediocrity"];
+
+var playAgain = false;
+
+var playAgain = confirm("Ready to be insulted by Ayn Rand?");
+
+while (playAgain === true) {
+    i = Math.floor(Math.random()*adj.length);
+    j = Math.floor(Math.random()*noun.length);
+alert("You " + adj[i] + " " + noun[j] + "!");
+var playAgain = confirm("Want to let Ayn Rand keep insulting you?");
+}
